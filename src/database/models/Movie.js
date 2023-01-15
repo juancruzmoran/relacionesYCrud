cc6435ae2322edd194d459b85c5566d28da60449
juancruzmoran <juancruzmoran@outlook.es>
2023-01-15 17:41:13 -0300

    relaciones

--- src/database/models/Movie.js
+++ src/database/models/Movie.js
@@ -37,6 +37,18 @@ module.exports = (sequelize, dataTypes) => {
     const Movie = sequelize.define(alias,cols,config);
 
     //Aquí debes realizar lo necesario para crear las relaciones con los otros modelos (Genre - Actor)
+    Movie.associate=(models)=>{
+        Movie.belongsTo(models.Genre,{
+            as:'genre',
+            foreignKey:'genre_id'
+        });
 
+        Movie.belongsToMany(models.Actor,{
+            as:'actors',
+            through:'actor_movie',
+            foreignKey:'actor_id',
+            otherKey:'actor_id'
+        })
+    }
     return Movie
 };
